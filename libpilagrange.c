ddbf51b1b3aea25274cf65d0958892f7d4aa5b92
Ubuntu <ubuntu@ip-172-31-1-168.us-east-2.compute.internal>
2023-11-10 17:17:13 +0000

    Modificamos el punto c añadiendo la funcion de lagrange

--- libpilagrange.c
+++ libpilagrange.c
@@ -1,2 +1,15 @@
-void lagrange(float x[], float y[], int n, float xp, float *yp);
+void lagrange(float x[], float y[], int n, float xp, float *yp){
+   int i, j;
+   float p;
+   *yp = 0;
+   for (i = 0; i < n; i++) {
+     p = 1;
+     for (j = 0; j < n; j++) {
+       if (i != j) {
+        p = p * (xp - x[j]) / (x[i] - x[j]);
+       }
+     }
+     *yp = *yp + p * y[i];
+    }
+}
 
